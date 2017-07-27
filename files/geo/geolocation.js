11d661c1db4b92c15e6d7b5e0929fb67933b221b
Timothy Kornish <TimmehK@Timothys-MacBook-Pro.local>
2017-07-27 14:16:51 -0600

    merge 3 geolocation added

---
+++ files/geo/geolocation.js
@@ -0,0 +1,62 @@
+import React, { Component } from 'react';
+import { View, Text } from 'react-native';
+
+class Geolocation extends Component {
+  constructor(props) {
+    super(props);
+
+    this.state = {
+      latitude: null,
+      longitude: null,
+      error: null
+    };
+    this.watchId = null
+
+    setInterval(this.location.bind(this),1000)
+  }
+
+  location(){
+    //console.log(this.watchId)
+    navigator.geolocation.clearWatch(this.watchId);
+
+    this.watchId = navigator.geolocation.watchPosition(
+
+      (position) => {
+         //console.log("looking for cache ", navigator.geolocation.clearWatch)
+        console.log("latitude: ",position.coords.latitude)
+        console.log("longitude: ",position.coords.longitude)
+        let obj = {
+          latitude: position.coords.latitude,
+          longitude: position.coords.longitude,
+          error: null
+        }
+        this.setState(obj);
+      },
+      (error) => this.setState({ error: error.message }),
+      { enableHighAccuracy: true, timeout: 10000, maximumAge: 1000},
+    );
+
+  }
+
+  componentDidMount() {
+    this.location()
+  }
+
+  componentWillUnmount() {
+    navigator.geolocation.clearWatch(this.watchId);
+  }
+
+  render() {
+    return (
+
+              <View style={{ position: 'relative', top: 70, flexGrow: 1, alignItems: 'center', justifyContent: 'center' }}>
+                <Text>Latitude: {this.state.latitude}</Text>
+                <Text>Longitude: {this.state.longitude}</Text>
+                {this.state.error ? <Text>Error: {this.state.error}</Text> : null}
+              </View>
+
+    );
+  }
+}
+
+export default Geolocation;
