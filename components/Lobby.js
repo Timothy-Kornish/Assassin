8b70a2ca8aacc606a72f0e7920e91bd351dde050
MacUser <quintinbruderer@yahoo.com>
2017-08-08 11:39:37 -0600

    reintegration for changes from recreation of master

--- components/Lobby.js
+++ components/Lobby.js
@@ -2,8 +2,8 @@ import React, {Component} from 'react'
 import {Button, View, Text} from 'react-native'
 import {connect} from 'react-redux'
 import {StackNavigator} from 'react-navigation'
-import JoinRoom from './JoinRoom'
-import CreateRoom from './CreateRoom'
+import JoinRoom from './Lobby-RoomComponents/JoinRoom'
+import CreateRoom from './Lobby-RoomComponents/CreateRoom'
 
 export default class Lobby extends Component {
   render(){
