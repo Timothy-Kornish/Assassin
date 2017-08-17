b3cd98065539e4007df1c3e743d1324f06b3f22a
Brenda Nickisch <mamagriz80@gmail.com>
2017-08-17 09:49:22 -0600

    Update credits.js

--- components/assets/credits.js
+++ components/assets/credits.js
@@ -1,18 +1,18 @@
 
 const creditString = 'Authentication/AutoLogin: Cian Hohman, Kelsey Iverson, Lauren Grinder \n\n' +
-'Server Logic: Timothy Kornish, Brenda Nickish, Cian Hohman \n\n' +
-'SQL Database: Timothy Kornish, Brenda Nickish, Cian Hohman \n\n' +
+'Server Logic: Timothy Kornish, Brenda Nickisch, Cian Hohman \n\n' +
+'SQL Database: Timothy Kornish, Brenda Nickisch, Cian Hohman \n\n' +
 'Game Monitor: Timothy Kornish, Thomas Elrod, Quinn Bruderer, Cian Hohman \n\n' +
 'Game HUD: Thomas Elrod, Quinn Bruderer \n\n' +
-'Location Monitor: Kelsey Iverson, Brenda Nickish, Quinn Bruderer \n\n' +
+'Location Monitor: Kelsey Iverson, Brenda Nickisch, Quinn Bruderer \n\n' +
 'Alerts/Push Notifications: Quinn Bruderer, Lauren Grinder \n\n' +
 'Front End Logic: Cian Hohman, Lauren Grinder, Thomas Elrod \n\n' +
 'Room Component: Timothy Kornish, Thomas Elrod, Quinn Bruderer \n\n' +
-'Death Room: Brenda Nickish, Kelsey Iverson, Lauren Grinder, Timothy Kornish \n\n' +
-'Story: Brenda Nickish, Lauren Grinder \n\n' +
-'Styling: Brenda Nickish, Quinn Bruderer, Shannon Dooling \n\n' +
-'Illustrations: Calvin Nickish, Quinn Bruderer \n\n' +
-'Graphics: Quinn Bruderer, Brenda Nickish \n\n' +
+'Death Room: Brenda Nickisch, Kelsey Iverson, Lauren Grinder, Timothy Kornish \n\n' +
+'Story: Brenda Nickisch, Lauren Grinder \n\n' +
+'Styling: Brenda Nickisch, Quinn Bruderer, Shannon Dooling \n\n' +
+'Illustrations: Calvin Nickisch, Quinn Bruderer \n\n' +
+'Graphics: Quinn Bruderer, Brenda Nickisch \n\n' +
 'Montana Code School: Cohort 6: Assassins'
 
 module.exports = creditString
